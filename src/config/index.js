f41b191995ab5f368285f7510ae6988a8bbfd243
oussama seffai <ousf.dev@gmail.com>
2024-06-16 20:55:02 +0100

    refactor(config): validate env file

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,34 @@
 require("dotenv").config();
+const joi = require("joi");
+
+const envVarsSchema = joi
+  .object()
+  .keys({
+    NODE_ENV: joi
+      .string()
+      .valid("production", "development", "test")
+      .required(),
+    PORT: joi.number().positive().default(4000),
+    WS_PORT: joi.number().positive().default(8080),
+    JWT_SECRET: joi.string().default("my-very-secret-key"),
+    REDIS_URL: joi.string().default("redis://localhost:6379"),
+    MONGODB_URI: joi.string().required(),
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
 
 module.exports = {
-  PORT: process.env.PORT || 4000,
-  WS_PORT: process.env.WS_PORT || 8080,
-  REDIS_URL: process.env.REDIS_URL || "redis://localhost:6379",
-  MONGODB_URI: process.env.MONGODB_URI,
-  JWT_SECRET: process.env.JWT_SECRET || "my-very-secret-key",
+  NODE_ENV: envVars.NODE_ENV,
+  PORT: envVars.PORT,
+  WS_PORT: envVars.WS_PORT,
+  JWT_SECRET: envVars.JWT_SECRET,
+  REDIS_URL: envVars.REDIS_URL,
+  MONGODB_URI: envVars.MONGODB_URI,
 };
